ad74ab7f7b0cc3cf3109819a9b4123d41b3bc3ff
Andrew Jones <joneseyit@protonmail.com>
2018-10-31 15:47:12 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,9 @@
 require "pry"
 class Dog
-  def initialize(name, breed)
+  def initialize(name, breed = "Mutt")
     @name = name
     @breed = breed
   end
 
-  def name(new_name)
-    @name = new_name
-  end
 
-  def breed(breed = "mutt")
-    @breed
-  end
 end
